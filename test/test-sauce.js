6e0c41c3eecc3d5de4d652023d8a7283c549a032
Arbi Hernowo <macbook@MacBooks-MacBook-Air.local>
2025-06-25 12:31:14 +0700

    add  hook and headless

--- test/test-sauce.js
+++ test/test-sauce.js
@@ -1,26 +1,43 @@
 const {Builder, By, until} = require('selenium-webdriver')
 const assert = require('assert');
 const { title } = require('process');
+const chrome = require('selenium-webdriver/chrome')
 
 describe('Saucedemo Test', function () {
     let driver;
 
-    it('Sort Descending', async function(){
-        driver = await new Builder().forBrowser('chrome').build();
+    beforeEach(async function () {
+        options = new chrome.Options();
+        options.addArguments("--headless");
 
+        driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
         await driver.get('https://www.saucedemo.com/');
+    });
 
+    afterEach(async function () {
+        await driver.quit();
+    });
+
+    it('Sukses Login', async function(){
         let inputUsername = await driver.findElement(By.css('[data-test="username"]'))
         let inputPassword = await driver.findElement(By.xpath('//*[@data-test="password"]'))
         let buttonLogin = await driver.findElement(By.className('submit-button btn_action'))
-        
-
         await inputUsername.sendKeys('standard_user')
         await inputPassword.sendKeys('secret_sauce')
         await buttonLogin.click()
 
         let buttonChart = await driver.findElement(By.xpath('//*[@id="shopping_cart_container"]'))
         await buttonChart.isDisplayed();
+
+    });
+
+    it('Descending Sort', async function(){
+        let inputUsername = await driver.findElement(By.css('[data-test="username"]'))
+        let inputPassword = await driver.findElement(By.xpath('//*[@data-test="password"]'))
+        let buttonLogin = await driver.findElement(By.className('submit-button btn_action'))
+        await inputUsername.sendKeys('standard_user')
+        await inputPassword.sendKeys('secret_sauce')
+        await buttonLogin.click()
 
         let fiturSort = await driver.findElement(By.css('[data-test="product-sort-container"'))
         await fiturSort.click()
@@ -31,8 +48,6 @@ describe('Saucedemo Test', function () {
         let activeOption = await driver.findElement(By.css('[data-test="active-option"'))
         let activeText = await activeOption.getText()
         assert.strictEqual(activeText, 'Name (Z to A)')
-
-        await driver.quit();
 
     });
 
